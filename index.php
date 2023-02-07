a22211c0a8fd1fbf32c048effe688cc1e60148a1
paponvith <paponvith_naluk@windowslive.com>
2023-02-08 03:00:54 +0700

    เปลี่ยนหน้าล็อคอิน

--- index.php
+++ index.php
@@ -22,16 +22,67 @@
 
 <body class="bg-gradient-primary">
 
-    <div class="container">
+    <section class="vh-100 gradient-custom">
+        <div class="container py-5 h-100">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card bg-dark text-white" style="border-radius: 1rem;">
+                        <div class="card-body p-5 text-center">
 
-        <!-- Outer Row -->
+                            <div class="mb-md-5 mt-md-4 pb-5">
+
+                                <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                                <p class="text-white-50 mb-5">Please enter your login and password!</p>
+
+                                <form class="user">
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="text" class="form-control form-control-user" id="Username" aria-describedby="emailHelp" placeholder="Enter Username">
+                                    </div>
+                                    <div class="form-outline form-white mb-4">
+                                        <input type="password" class="form-control form-control-user" id="user_password" placeholder="Password">
+                                    </div>
+
+                                    <a href="#" class="btn btn-outline-light btn-lg px-5" onclick="login()">
+                                        Login
+                                    </a>
+
+
+                                </form>
+
+
+
+                                <!-- <p class="small mb-5 pb-lg-2"><a class="text-white-50" href="#!">Forgot password?</a></p> -->
+
+                                <!-- <button class="btn btn-outline-light btn-lg px-5" type="submit">Login</button> -->
+
+                                <div class="d-flex justify-content-center text-center mt-4 pt-1">
+                                    <a href="#!" class="text-white"><i class="fab fa-facebook-f fa-lg"></i></a>
+                                    <a href="#!" class="text-white"><i class="fab fa-twitter fa-lg mx-4 px-2"></i></a>
+                                    <a href="#!" class="text-white"><i class="fab fa-google fa-lg"></i></a>
+                                </div>
+
+                            </div>
+
+                            <!-- <div>
+                                <p class="mb-0">Don't have an account? <a href="#!" class="text-white-50 fw-bold">Sign Up</a>
+                                </p>
+                            </div> -->
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- <div class="container">
         <div class="row justify-content-center">
 
             <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
                     <div class="card-body p-0">
-                        <!-- Nested Row within Card Body -->
+                        
                         <div class="row">
                             <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
                             <div class="col-lg-6">
@@ -63,7 +114,7 @@
 
         </div>
 
-    </div>
+    </div> -->
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
